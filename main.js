28bcb5ea1e6b538ed66324aed0eec44cc01119b0
Mateo de Mayo <mateodemayo@gmail.com>
2020-08-16 23:29:00 -0300

    Implement simple Vector class

--- main.js
+++ main.js
@@ -6,35 +6,57 @@
 
 // TODO: Remove this eslint switch
 // eslint-disable-next-line max-classes-per-file
+const V = class Vector {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  static add(v, w) {
+    if (typeof w === "number") return new V(v.x + w, v.y + w)
+    return new V(v.x + w.x, v.y + w.y)
+  }
+
+  static rot(v, a, degrees = true) {
+    // Rotates vector v in degrees (or radians if degrees = false)
+    const radians = degrees ? a * (Math.PI / 180) : a
+    const [sina, cosa] = [Math.sin(radians), Math.cos(radians)]
+    return new V(cosa * v.x - sina * v.y, sina * v.x + cosa * v.y)
+  }
+
+  static get zero() {
+    return new V(0, 0)
+  }
+
+  toString() {
+    return `(${this.x}, ${this.y})`
+  }
+}
+const P = (x, y) => new V(x, y) // Short Vector (Point) construction
+
+// eslint-disable-next-line max-classes-per-file
 class Actor {
   constructor({ canvas, context }, x, y) {
     this.context = context
     this.canvas = canvas
-    this.x = x
-    this.y = y
-    this.velx = 1
-    this.vely = 1
+    this.location = P(x, y)
+    this.velocity = P(1, 1)
   }
 
   update() {
-    if (
-      (this.x <= 0 && this.velx < 0) ||
-      (this.x >= this.canvas.width && this.velx > 0)
-    )
-      this.velx *= -1
-    if (
-      (this.y <= 0 && this.vely < 0) ||
-      (this.y >= this.canvas.width && this.vely > 0)
-    )
-      this.vely *= -1
-    this.x += this.velx
-    this.y += this.vely
+    const { x, y } = this.location
+    const { x: vx, y: vy } = this.velocity
+    const { width } = this.canvas
+    if ((x <= 0 && vx < 0) || (x >= width && vx > 0)) this.velocity.x = -vx
+    if ((y <= 0 && vy < 0) || (y >= width && vy > 0)) this.velocity.y = -vy
+    this.location.x += this.velocity.x
+    this.location.y += this.velocity.y
   }
 
   render() {
     const c = this.context
     c.save()
-    c.translate(this.x, this.y)
+    c.translate(this.location.x, this.location.y)
     c.fillStyle = "white"
     c.fillRect(0, 0, 16, 16)
     c.restore()
@@ -46,7 +68,7 @@ class AttractionPoint extends Actor {
     const c = this.context
     c.save()
     c.beginPath()
-    c.translate(this.x, this.y)
+    c.translate(this.location.x, this.location.y)
     c.fillStyle = "deeppink"
     c.arc(0, 0, 4, 0, Math.PI * 2)
     c.fill()
