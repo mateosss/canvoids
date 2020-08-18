ee510c1e98bde2d7829b2dc43ee961797b3ca8d1
Mateo de Mayo <mateodemayo@gmail.com>
2020-08-17 22:34:49 -0300

    Add center of mass attraction rule

--- main.js
+++ main.js
@@ -22,6 +22,33 @@ const V = class Vector {
     ;[this.x, this.y] = [x, y]
   }
 
+  static sub(v, w) {
+    if (typeof w === "number") return new V(v.x - w, v.y - w)
+    return new V(v.x - w.x, v.y - w.y)
+  }
+
+  sub(w) {
+    const { x, y } = V.sub(this, w)
+    ;[this.x, this.y] = [x, y]
+  }
+
+  static scale(v, k) {
+    return new V(v.x * k, v.y * k)
+  }
+
+  scale(k) {
+    const { x, y } = V.scale(this, k)
+    ;[this.x, this.y] = [x, y]
+  }
+
+  static mul(v, k) {
+    V.scale(v, k)
+  }
+
+  mul(k) {
+    this.scale(k)
+  }
+
   static rot(v, a, degrees = true) {
     // Rotates vector v in degrees (or radians if degrees = false)
     const radians = degrees ? a * (Math.PI / 180) : a
@@ -46,20 +73,31 @@ const P = (x, y) => new V(x, y) // Short Vector (Point) construction
 
 // TODO: Remove this eslint switch
 // eslint-disable-next-line max-classes-per-file
-class Actor {
-  constructor({ canvas, context }, x, y) {
+class Boid {
+  constructor({ canvas, context, boids }, x, y) {
     this.context = context
     this.canvas = canvas
+    this.boids = boids
     this.location = P(x, y)
     this.velocity = P(1, 1)
   }
 
   update() {
+    // Go towards center of mass
+    const { boids } = this
+    const locationMean = P(0, 0)
+    for (const boid of boids) if (boid !== this) locationMean.add(boid.location)
+    locationMean.scale(1 / boids.length)
+    const scaledLocMean = V.scale(V.sub(locationMean, this.location), 0.01)
+    this.velocity.add(scaledLocMean)
+
+    // DVD Bounce
     const { x, y } = this.location
     const { x: vx, y: vy } = this.velocity
     const { width } = this.canvas
     if ((x <= 0 && vx < 0) || (x >= width && vx > 0)) this.velocity.x = -vx
     if ((y <= 0 && vy < 0) || (y >= width && vy > 0)) this.velocity.y = -vy
+
     this.location.add(this.velocity)
   }
 
@@ -68,12 +106,12 @@ class Actor {
     c.save()
     c.translate(this.location.x, this.location.y)
     c.fillStyle = "white"
-    c.fillRect(0, 0, 16, 16)
+    c.fillRect(0, 0, 4, 4)
     c.restore()
   }
 }
 
-class AttractionPoint extends Actor {
+class AttractionPoint extends Boid {
   render() {
     const c = this.context
     c.save()
@@ -88,14 +126,14 @@ class AttractionPoint extends Actor {
 
 class Game {
   constructor() {
-    this.actors = []
+    this.boids = []
     this.canvas = document.querySelector("#canvas")
     this.attractionPoint = null
     this.context = this.canvas.getContext("2d")
     // Initialize actors
     for (let i = 0; i < 8; i++) {
-      const actor = new Actor(this, Math.floor(Math.random() * 256), 1 + i * 32)
-      this.actors.push(actor)
+      const actor = new Boid(this, Math.floor(Math.random() * 256), 1 + i * 32)
+      this.boids.push(actor)
     }
   }
 
@@ -117,7 +155,7 @@ class Game {
     c.fillRect(0, 0, this.canvas.width, this.canvas.height)
 
     this.attractionPoint?.render()
-    for (const actor of this.actors) {
+    for (const actor of this.boids) {
       actor.update()
       actor.render()
     }
